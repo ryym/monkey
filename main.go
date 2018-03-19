f70a8c789fdc337fff5658ea3719d171a4d563bc
ryym <ryym.64@gmail.com>
2018-03-19 21:41:19 +0900

    Fix typo

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! Thi si the Monkey programming language!\n", user.Username)
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
 	fmt.Println("Feel free to type in commands")
 	repl.Start(os.Stdin, os.Stdout)
 }
