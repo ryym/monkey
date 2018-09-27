c22c0d386f00eafc13162a524a20cf21debcdd53
ryym <ryym.64@gmail.com>
2018-09-27 15:47:02 +0900

    02: Fix incorrect ast.FunctionLiteral String func

--- ast/ast.go
+++ ast/ast.go
@@ -244,7 +244,9 @@ func (fl *FunctionLiteral) String() string {
 	out.WriteString(fl.TokenLiteral())
 	out.WriteString("(")
 	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {")
 	out.WriteString(fl.Body.String())
+	out.WriteString("}")
 
 	return out.String()
 }
